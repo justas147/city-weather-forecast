01d41ba2dc67192a437b645f3f427329b6f37a5d
Justas <jradkevicius@gmail.com>
2021-01-16 15:21:41 +0200

    Mapping configuration added

---
+++ city-weather-forecast-API/Configurations/AutoMapperConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using city_weather_forecast_API.Models;
+using city_weather_forecast_API.Models.DTO;
+
+namespace city_weather_forecast_API.Configurations
+{
+    public class AutoMapperConfiguration : Profile
+    {
+        public AutoMapperConfiguration()
+        {
+            CreateMap<City, CityDetailsDTO>()
+                .ForMember(dest => dest.MaxTemperature, opt => opt.MapFrom<CityMaxTemperatureResolver>())
+                .ForMember(dest => dest.MinTemperature, opt => opt.MapFrom<CityMinTemperatureResolver>());
+        }
+    }
+}
---
+++ city-weather-forecast-API/Configurations/AutoMapperResolvers/CityMinTemperatureResolver.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using city_weather_forecast_API.Models;
+using city_weather_forecast_API.Models.DTO;
+using city_weather_forecast_API.Services.ServiceInterfaces;
+
+namespace city_weather_forecast_API.Configurations
+{
+    public class CityMinTemperatureResolver : IValueResolver<City, CityDetailsDTO, string>
+    {
+        private readonly IForecastApi _forecastApiService;
+
+        public CityMinTemperatureResolver(IForecastApi forecastApiService)
+        {
+            _forecastApiService = forecastApiService;
+        }
+
+        public string Resolve(City source, CityDetailsDTO destination, string destMember, ResolutionContext context)
+        {
+            return _forecastApiService.GetTemperature(source.PlaceCode, Services.TemperatureExtremum.Min).Result;
+        }
+    }
+}
